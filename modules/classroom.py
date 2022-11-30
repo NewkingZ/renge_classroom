31bde08bc5c1b365d49458d0fa69cec10f8a1e7f
Antoine Desktop <Newantoine@msn.com>
2022-11-29 23:05:26 -0500

    updated gui

--- modules/classroom.py
+++ modules/classroom.py
@@ -62,19 +62,71 @@ class Classroom:
 
 		# Set-up the Renge Frame:
 		self.frame_renge.grid_propagate(False)
-		self.frame_renge.rowconfigure(10, weight=2)
+		self.frame_renge.rowconfigure([10, 20], weight=2)
 		self.frame_renge.columnconfigure([10, 20], weight=2)
 
 		# Set-up the picture and introduction
 		welcome_pic = tk.PhotoImage(file=get_image_path(styles.PIC_RENGE_PATH, "renge_welcome.png"))
 		self.renge_welcome = tk.Label(self.frame_renge, image=welcome_pic)
 		self.renge_welcome.image = welcome_pic
-		self.renge_welcome.grid(row=10, column=10, sticky="ns")
+		self.renge_welcome.grid(row=10, column=10, rowspan=11, sticky="ns")
 
 		tk.Label(self.frame_renge, text="Nyanpasu!", font=styles.TITLE_FONT).grid(row=10, column=20, sticky="news")
+		tk.Label(self.frame_renge, text="Time to learn some\nJapanese Mutha Fucka").\
+			grid(row=20, column=20, sticky="new")
 
-		# Set-up the Subject
+		# Set-up the Subject Frame
 		self.frame_subject.grid_propagate(False)
+		self.frame_subject.rowconfigure([10, 90], weight=2)
+		self.frame_subject.rowconfigure([20, 30, 40], weight=1)
+		self.frame_subject.columnconfigure([10, 20, 30], weight=2)
+
+		# Set-up the title and options
+		tk.Label(self.frame_subject, text="Select subject to test!", font=styles.SUBTITLE_FONT).\
+			grid(row=10, column=10, columnspan=11, sticky="news")
+
+		self.subject_var = tk.IntVar()
+		self.subject_options_hiragana = tk.Radiobutton(self.frame_subject, text="Hiragana", variable=self.subject_var,
+													   value=1)
+		self.subject_options_hiragana.grid(row=20, column=20, sticky="nws")
+		self.subject_options_katakana = tk.Radiobutton(self.frame_subject, text="Katakana", variable=self.subject_var,
+													   value=2)
+		self.subject_options_katakana.grid(row=30, column=20, sticky="nws")
+		self.subject_options_kanji = tk.Radiobutton(self.frame_subject, text="Kanji", variable=self.subject_var,
+													   value=3)
+		self.subject_options_kanji.grid(row=40, column=20, sticky="nws")
+
+		# Set-up the count frame
+		self.frame_count.grid_propagate(False)
+		self.frame_count.rowconfigure([10, 90], weight=2)
+		self.frame_count.rowconfigure([20, 30, 40, 50], weight=1)
+		self.frame_count.columnconfigure([10, 20, 30], weight=2)
+
+		# Create and add number of questions options
+		tk.Label(self.frame_count, text="Select number of questions", font=styles.SUBTITLE_FONT).\
+			grid(row=10, column=10, columnspan=11, sticky="news")
+
+		self.count_var = tk.IntVar()
+		self.count_options_10 = tk.Radiobutton(self.frame_count, text="10", variable=self.count_var,
+													   value=1)
+		self.count_options_10.grid(row=20, column=20, sticky="nws")
+
+		self.count_options_25 = tk.Radiobutton(self.frame_count, text="25", variable=self.count_var,
+													   value=2)
+		self.count_options_25.grid(row=30, column=20, sticky="nws")
+
+		self.count_options_50 = tk.Radiobutton(self.frame_count, text="50", variable=self.count_var,
+													   value=3)
+		self.count_options_50.grid(row=40, column=20, sticky="nws")
+
+		self.count_options_100 = tk.Radiobutton(self.frame_count, text="100", variable=self.count_var,
+													   value=4)
+		self.count_options_100.grid(row=50, column=20, sticky="nws")
+
+
+
+
+
 
 
 
--- modules/styles.py
+++ modules/styles.py
@@ -4,3 +4,4 @@ PIC_RENGE_PATH = './assets/renge_pics/'
 
 
 TITLE_FONT = ("calibri", 20)
+SUBTITLE_FONT = ("calibri", 15)
